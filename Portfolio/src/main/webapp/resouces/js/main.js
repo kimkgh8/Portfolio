695c86c48ad0a79ce4448b3e2c53082493d97b09
KimGuenho <kimkgh8@gmail.com>
2021-07-04 23:53:30 +0900

    아래의 작업 실시.   footer아이콘화   화면 최적화

--- Portfolio/src/main/webapp/resouces/js/main.js
+++ Portfolio/src/main/webapp/resouces/js/main.js
@@ -41,3 +41,14 @@ window.onload = function() {
 		});
 	});
 }
+
+
+function toggleCodes(on) {
+	var obj = document.getElementById('icons');
+
+	if (on) {
+		obj.className += ' codesOn';
+	} else {
+		obj.className = obj.className.replace(' codesOn', '');
+	}
+}
