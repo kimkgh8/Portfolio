47c510eade20b232edc3a3b5341a5ff97c455f23
KimGuenho <kimkgh8@gmail.com>
2021-07-19 21:01:25 +0900

    DB 조회 테스트추가

--- Portfolio/src/test/java/com/take/portfolio/MapperTests.java
+++ Portfolio/src/test/java/com/take/portfolio/MapperTests.java
@@ -1,8 +1,11 @@
 package com.take.portfolio;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,6 +40,33 @@ public class MapperTests {
 
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testMultipleInsert() {
+		for (int i = 2; i <= 50; i++) {
+			BoardDTO params = new BoardDTO();
+			params.setTitle(i + "번 게시글 제목");
+			params.setContent(i + "번 게시글 내용");
+			params.setWriter(i + "번 게시글 작성자");
+			boardMapper.insertBoard(params);
+		}
+	}
+	
+	@Test
+	public void testSelectList() {
+		int boardTotalCount = boardMapper.selectBoardTotalCount();
+		if (boardTotalCount > 0) {
+			List<BoardDTO> boardList = boardMapper.selectBoardList();
+			if (CollectionUtils.isEmpty(boardList) == false) {
+				for (BoardDTO board : boardList) {
+					System.out.println("=========================");
+					System.out.println(board.getTitle());
+					System.out.println(board.getContent());
+					System.out.println(board.getWriter());
+					System.out.println("=========================");
+				}
+			}
 		}
 	}
 }
